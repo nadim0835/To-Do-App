2b68a451fe9778a6304488cb3592f3f3a4bd8e79
nadim0835 <nadim0835@gmail.com>
2023-11-25 14:29:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let ul =document.querySelector('ul')
+
+ 
+// button.addEventListener('click',function(){
+//   let li =document.createElement('li')
+//   let ivalue=input.value;
+//   li.innerHTML=ivalue;
+//   ul.appendChild(li)
+// })
+button.addEventListener('click',function(){
+  let ivalue=input.value;
+  if(ivalue===''){
+    alert("You have must write something")
+  }
+  else{
+    let li =document.createElement('li')
+    li.innerHTML=ivalue;
+    ul.appendChild(li)
+  }
+ 
+})
+
+
